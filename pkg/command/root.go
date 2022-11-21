1c964b631ca16e9ec85e453fac98b325e89f43e0
Vui Lam <vui@vmware.com>
2022-11-20 18:17:52 -0800

    Update root command to use PluginSupplier
    The PluginSupplier interface provides the CLI with a list of
    plugins to integrate with it.
    
    Added unit tests to verify behavior of interacting with subcommands
    provided by shell scripts doubling as plugins.

--- pkg/command/root.go
+++ pkg/command/root.go
@@ -7,11 +7,32 @@ package command
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/vmware-tanzu/tanzu-plugin-runtime/plugin"
+
+	"github.com/vmware-tanzu/tanzu-cli/pkg/catalog"
 	"github.com/vmware-tanzu/tanzu-cli/pkg/cli"
 )
 
+type DummyPluginSupplier struct {
+}
+
+// GetInstalledPlugins returns plugins for the dummy supplier.
+// TODO(vuil) : delete and replace with actual implementation
+func (s *DummyPluginSupplier) GetInstalledPlugins() ([]*cli.PluginInfo, error) {
+	plugins := make([]*cli.PluginInfo, 0)
+	pi := &cli.PluginInfo{
+		Name:             "fakefoo",
+		Description:      "Fake foo",
+		Group:            plugin.SystemCmdGroup,
+		Aliases:          []string{"ff"},
+		InstallationPath: "/opt/tanzu/tmpfoo.info.cmds.goodbad.usage",
+	}
+	plugins = append(plugins, pi)
+	return plugins, nil
+}
+
 // NewRootCmd creates a root command.
-func NewRootCmd() (*cobra.Command, error) {
+func NewRootCmd(ps catalog.PluginSupplier) (*cobra.Command, error) {
 	var rootCmd = &cobra.Command{
 		Use: "tanzu",
 		// Don't have Cobra print the error message, the CLI will
@@ -25,12 +46,24 @@ func NewRootCmd() (*cobra.Command, error) {
 		newVersionCmd(),
 	)
 
+	if ps == nil {
+		ps = &DummyPluginSupplier{}
+	}
+
+	plugins, err := ps.GetInstalledPlugins()
+	if err != nil {
+		return nil, err
+	}
+	for _, plugin := range plugins {
+		rootCmd.AddCommand(cli.GetCmd(plugin))
+	}
+
 	return rootCmd, nil
 }
 
 // Execute executes the CLI.
 func Execute() error {
-	root, err := NewRootCmd()
+	root, err := NewRootCmd(nil)
 	if err != nil {
 		return err
 	}
